805bede2f4fd8b3a726ac3c95ee3ac7fda6987dc
JKapturkiewicz <kuba.kapek@gmail.com>
2024-03-16 11:45:49 +0100

    chore: implement layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,22 @@
-import Header from '../components/Header/Header'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.scss'
-import WelcomeScreen from '../components/WelcomeScreen/WelcomeScreen'
-import CapturePhoto from '@/components/CapturePhoto/CapturePhoto'
-
+import Header from '../components/Header/Header';
+import { Inter } from 'next/font/google';
+import styles from '@/styles/Home.module.scss';
+import WelcomeScreen from '../components/WelcomeScreen/WelcomeScreen';
+import CapturePhoto from '@/components/CapturePhoto/CapturePhoto';
+import Layout from '@/components/Layout/Layout';
 
 const inter = Inter({
-	subsets: ['latin'],
-	weight: ['400', '500'],
-	variable: '--font-inter',
-})
+  subsets: ['latin'],
+  weight: ['400', '500'],
+  variable: '--font-inter',
+});
 
 export default function Home() {
-	return (
-		<>
-		<Header />
-		<WelcomeScreen />
-     <CapturePhoto />
-		</>
-	)
+  return (
+    <Layout>
+      <Header />
+      <WelcomeScreen />
+      <CapturePhoto />
+    </Layout>
+  );
 }
